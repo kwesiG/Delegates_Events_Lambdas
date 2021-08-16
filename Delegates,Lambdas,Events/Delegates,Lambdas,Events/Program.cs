a4ccb4e48020d2816a1493fb22e90ff8f7b00cf7
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-08-16 20:57:08 +0100

    Delegates del1 and del2 created and used with the DoWork method to decouple the code

---
+++ Delegates,Lambdas,Events/Delegates,Lambdas,Events/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegates_Lambdas_Events
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            DoWork(del2);
+            Console.ReadLine();
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 Called " + hours.ToString());
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 Called " + hours.ToString());
+        }
+    }
+}
