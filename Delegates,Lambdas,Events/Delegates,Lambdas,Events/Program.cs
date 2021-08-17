07391ca6ef2cfb668ea51a917f70eda8caf97d36
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-08-17 20:55:54 +0100

    Multi cast delegate and the invocation list.

--- Delegates,Lambdas,Events/Delegates,Lambdas,Events/Program.cs
+++ Delegates,Lambdas,Events/Delegates,Lambdas,Events/Program.cs
@@ -9,7 +9,13 @@ namespace Delegates_Lambdas_Events
         {
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
             WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            DoWork(del2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            //del1 += del2;
+            //del1 += del3;
+            del1 += del2 + del3;
+
+            DoWork(del1);
             Console.ReadLine();
         }
 
@@ -26,6 +32,10 @@ namespace Delegates_Lambdas_Events
         static void WorkPerformed2(int hours, WorkType workType)
         {
             Console.WriteLine("WorkPerformed2 Called " + hours.ToString());
+        }
+        static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed3 Called " + hours.ToString());
         }
     }
 }
